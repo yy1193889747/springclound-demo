47a5cd2c9bad563e917b6aa93eaaf1d8e282379a
yy1193889747 <1193889747@qq.com>
2018-01-25 10:24:44 +0800

    Initial commit

---
+++ eurekaserver/src/main/java/com/ocly/EurekaserverApplication.java
@@ -0,0 +1,13 @@
+package com.ocly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaserverApplication.class, args);
+	}
+}
