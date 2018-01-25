a26ef8ad28f99a243845af3e8ba2483a10966667
yy1193889747 <1193889747@qq.com>
2018-01-25 17:23:52 +0800

    bus

---
+++ config-client/src/main/java/com/ocly/ConfigClientApplication.java
@@ -0,0 +1,27 @@
+package com.ocly;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RefreshScope
+@EnableEurekaClient
+@RestController
+@SpringBootApplication
+public class ConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientApplication.class, args);
+	}
+
+	@Value("${foo}")
+	String foo;
+
+	@GetMapping("/")
+	public String getDev(){
+		return foo;
+	}
+}
